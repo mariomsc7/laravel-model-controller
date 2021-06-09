c855f044867fe0abee0428a5e8009b42c67d2822
mariomsc7 <mariomiscischia@gmail.com>
2021-06-09 14:48:02 +0200

    start+header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base')
+
+@section('content')
+
+    <section>
+        <div class="cards">
+            
+        </div>
+    </section>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="header container text-center">
+        <h1>MoviesDatabase</h1>
+        <nav class="navbar flex">
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
